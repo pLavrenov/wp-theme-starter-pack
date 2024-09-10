6afd54f13f1c8f9d33f514180ad0e5870e4f2da2
fleeks <lavrenov.pavel@gmail.com>
2024-09-10 17:59:09 +0300

    upd

--- header.php
+++ header.php
@@ -12,9 +12,35 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!--[if lt IE 9]>
-	<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+    <?php
+    if (!$meta_title = get_field('meta_title')) $meta_title = get_field('meta_title', 'option');
+    if ($meta_title) { ?>
+        <title><?php echo $meta_title ?></title>
+        <meta property="og:title" content="<?php echo $meta_title ?>">
+        <meta name="twitter:title" content="<?php echo $meta_title ?>">
+    <?php } ?>
+
+    <?php
+    if (!$meta_description = get_field('meta_description')) $meta_description = get_field('meta_description', 'option');
+    if ($meta_description) { ?>
+        <meta name="description" content="<?php echo $meta_description ?>">
+        <meta property="og:description" content="<?php echo $meta_description ?>">
+        <meta name="twitter:description" content="<?php echo $meta_description ?>">
+    <?php } ?>
+
+    <?php
+    if (!$meta_image = get_field('meta_image')) $meta_image = get_field('meta_image', 'option');
+    if ($meta_image) { ?>
+        <meta property="og:image" content="<?php echo $meta_image ?>">
+        <meta name="twitter:image" content="<?php echo $meta_image ?>">
+    <?php } ?>
+
+    <meta property="og:url" content="<?php echo home_url($wp->request) ?>">
+    <meta property="twitter:url" content="<?php echo home_url($wp->request) ?>">
+    <meta property="twitter:domain" content="fleeks.ru">
+    <meta property="og:type" content="website">
+    <meta name="twitter:card" content="summary_large_image">
+
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- includes/helpers/include_helper_func.php
+++ includes/helpers/include_helper_func.php
@@ -8,7 +8,7 @@
  * @param string $size The size of the image to retrieve. Defaults to 'thumbnail'.
  * @return string The URL of the image attachment.
  */
-function get_images($id, $size = 'thumbnail') {
+function get_image($id, $size = 'thumbnail') {
     $id = get_post_thumbnail_id($id);
     return wp_get_attachment_image_src( $id , $size)[0];
 }
@@ -71,4 +71,30 @@ function build_tree( array &$elements, $parentId = 0 )
         }
     }
     return $branch;
+}
+
+/**
+ * Получает объект меню по его ID.
+ *
+ * @param int $id The ID of the navbar menu to retrieve.
+ * @return array The navbar object, with child items grouped with their parents.
+ */
+function get_menu($id) {
+    $navbar_items = wp_get_nav_menu_items($id);
+    $child_items = [];
+    // pull all child menu items into separate object
+    foreach ($navbar_items as $key => $item) if ($item->menu_item_parent) {
+        array_push($child_items, $item);
+        unset($navbar_items[$key]);
+    }
+    // push child items into their parent item in the original object
+    foreach ($navbar_items as $item)
+        foreach ($child_items as $key => $child)
+            if ($child->menu_item_parent == $item->post_name) {
+                if (!$item->child_items) $item->child_items = [];
+                array_push($item->child_items, $child);
+                unset($child_items[$key]);
+            }
+    // return navbar object where child items are grouped with parents
+    return $navbar_items;
 }
